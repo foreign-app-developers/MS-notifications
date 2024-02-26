77757adb92663fa8e6d216ca86e40bee7ed02b93
FEL <Vovafelinger75@gmail.com>
2024-02-26 19:18:59 +0700

    refactoring

--- app/src/Controller/NotificationController.php
+++ app/src/Controller/NotificationController.php
@@ -5,33 +5,43 @@ namespace App\Controller;
 use App\DBAL\Types\NotificationTypes;
 use App\Entity\Notification;
 use App\Repository\NotificationRepository;
-use http\Client\Request;
+use App\Service\TgService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/api/notification', name: 'app_notification')]
 class NotificationController extends AbstractController
 {
 
-    #[Route('/add', name: 'add_notification',methods: "POST")]
-    public function addNotification( NotificationRepository $repo):JsonResponse
+    #[Route('/send', name: 'add_notification',methods: "POST")]
+    public function sendNotification(NotificationRepository $repo, Request $request, TgService $tgService):JsonResponse
     {
+        $data = json_decode($request->getContent(), true);
         $notification = new Notification();
-        $notification->setType(NotificationTypes::SMS);
-        $notification->setContent("123");
-        $notification->setFromVal("1");
+        $notification->setContent($data['content']);
+        $notification->setFromVal('asd');
         $notification->setMoment(new \DateTime());
-        $notification->setTitle("123");
-        $notification->setIsReaded(true);
+        $notification->setTitle($data['title']);
+        $notification->setIsReaded(false);
         $notification->setTimeToSend(new \DateTime());
-        $notification->setToVal("asd");
-
-        $repo->save($notification, True);
+        $notification->setToVal($data['to']);
+        if($data['type'] == 'sms'){
+            $notification->setType(NotificationTypes::SMS);
+        }
+        if($data['type'] == 'email'){
+            $notification->setType(NotificationTypes::EMAIL);
+        }
+        if($data['type'] == 'tg'){
+            $notification->setType(NotificationTypes::TG);
+            $tgService->sendNotification($notification);
+            $repo->save($notification, True);
+        }
 
         return $this->json([
             'data' => $notification,
-            'message' => 'Уведомление успешно создан!',
+            'message' => 'Уведомление успешно отправлено!',
         ]);
     }
 

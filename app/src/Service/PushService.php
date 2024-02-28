a4d33908c349d64889e501186f4932b8e256230f
FEL <Vovafelinger75@gmail.com>
2024-02-29 01:13:01 +0700

    добавил кристинин телеграм

--- app/src/Service/PushService.php
+++ app/src/Service/PushService.php
@@ -4,30 +4,85 @@ namespace App\Service;
 
 use App\Entity\Notification;
 use App\NotificationInterface;
-use Symfony\Component\Notifier\Bridge\Firebase\Notification\AndroidNotification;
-use Symfony\Component\Notifier\ChatterInterface;
-use Symfony\Component\Notifier\Message\ChatMessage;
+use Google\Exception;
+use Google_Client;
+use Google_Exception;
+use Google_Service_FirebaseCloudMessaging;
+
 
 class PushService implements NotificationInterface
 {
-    private $chatter;
 
-    public function __construct(ChatterInterface $chatter)
+    function getOATHToken()
     {
-        $this->chatter = $chatter;
+        $client = new Google_Client();
+        try {
+            $client->setAuthConfig(__DIR__ . '/PrivateKey.json');
+            $client->addScope(Google_Service_FirebaseCloudMessaging::CLOUD_PLATFORM);
+
+            $savedTokenJson = $this->readSavedToken();
+
+            if ($savedTokenJson) {
+                $client->setAccessToken($savedTokenJson);
+                $accessToken = $savedTokenJson;
+                if ($client->isAccessTokenExpired()) {
+                    $accessToken = $this->generateToken($client);
+                    $client->setAccessToken($accessToken);
+                }
+            } else {
+                $accessToken = $this->generateToken($client);
+                $client->setAccessToken($accessToken);
+            }
+
+            return $accessToken["access_token"];
+
+
+        } catch (Google_Exception|Exception $e) {}
+        return false;
     }
-    public function sendNotification(Notification $notification): string
+
+    function readSavedToken() {
+        $tk = @file_get_contents(__DIR__ . '/token.cache');
+        if ($tk) return json_decode($tk, true); else return false;
+    }
+    function writeToken($tk): void
     {
-        $chatMessage = new ChatMessage($notification->getContent());
-        $chatId = $_ENV['PUSH_' . $notification->getToVal()];
-        $androidOptions = (new AndroidNotification($chatId, []))
-            ->tag('myNotificationId')
-            ->title($notification->getTitle())
-        ;
-        $chatMessage->options($androidOptions);
-        $this->chatter->send($chatMessage);
-        return "success";
+        file_put_contents(__DIR__ . '/token.cache',$tk);
+    }
+
+    function generateToken($client)
+    {
+        $client->fetchAccessTokenWithAssertion();
+        $accessToken = $client->getAccessToken();
+
+        $tokenJson = json_encode($accessToken);
+        $this->writeToken($tokenJson);
+
+        return $accessToken;
+    }
+
+
+    function sendNotification(Notification $notification): void
+    {
+        $accessToken = $this->getOATHToken();
+        $token = $_ENV['PUSH_' . $notification->getToVal()];
+        $payload = ["message" => ["token" => $token, "notification"=>["title" => $notification->getTitle(), "body"=> $notification->getContent()]]];
+        $postdata = json_encode($payload);
+
+        $ch = curl_init('https://fcm.googleapis.com/v1/projects/'. $_ENV['GOOGLE_PROJECT_NAME'] .'/messages:send');
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+            'Authorization: Bearer ' . $accessToken,
+        ]);
+
+        curl_exec($ch);
+        curl_close($ch);
+
 
     }
+
 
 }
